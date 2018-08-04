cb02ee6d45b074bb43c2658aec95c7b4b69cebb1
stebal <stephane.baldino@gmail.com>
2018-08-04 20:09:27 +0200

    after part 12

--- app/src/main/java/com/stebal/plantquiz/MainActivity.kt
+++ app/src/main/java/com/stebal/plantquiz/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.stebal.plantquiz
 
+import android.media.Image
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,9 +19,55 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        /* Toast.makeText(this, "THE ONCREATE METHOD IS CALLED", Toast.LENGTH_SHORT).show()
+
+        val myPlant: Plant = Plant(genus = "", species = "", cultivar = "", common = "", pictureName = "", description = "", difficulty = 0, id = 0 )
+
+        myPlant.plantName = "" */
+
+        var flower = Plant()
+        var tree = Plant()
+
+        var collectionOfPlants: ArrayList<Plant> = ArrayList()
+        collectionOfPlants.add(flower)
+        collectionOfPlants.add(tree)
+
+    }
+
+    fun button1IsClicked(buttonView: View) {
+
+        Toast.makeText(this, "THE Button 1 is Clicked", Toast.LENGTH_SHORT).show()
+
+
+    }
+
+    fun button2IsClicked(buttonView: View) {
+
+        Toast.makeText(this, "THE Button 2 is Clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    fun button3IsClicked(buttonView: View) {
+
+        Toast.makeText(this, "THE Button 3 is Clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    fun button4IsClicked(buttonView: View) {
+
+        Toast.makeText(this, "THE Button 4 is Clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    inner class DownloadingPlantTask: AsyncTask<String, Int, List<Plant>>() {
+
+        override fun doInBackground(vararg param: String?): List<Plant>? {
+            // can access background thread. Not user interface.
+
+            return null
+        }
+
+        override fun onPostExecute(result: List<Plant>?) {
+            super.onPostExecute(result)
+
+            // can access user interface thread. Not background thread
         }
     }
 
