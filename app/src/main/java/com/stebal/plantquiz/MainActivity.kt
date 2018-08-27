7abf5c2c8950d1a862157b4d477b82bc6b8fe79f
stebal <stephane.baldino@gmail.com>
2018-08-27 20:43:48 +0200

    open camera intent

--- app/src/main/java/com/stebal/plantquiz/MainActivity.kt
+++ app/src/main/java/com/stebal/plantquiz/MainActivity.kt
@@ -1,18 +1,30 @@
 package com.stebal.plantquiz
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.media.Image
 import android.os.AsyncTask
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var cameraButton: Button? = null
+    private var photoGalleryButton: Button? = null
+    private var imageTaken: ImageView? = null
+
+    val OPEN_CAMERA_BUTTON_REQUEST_ID = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,13 +37,39 @@ class MainActivity : AppCompatActivity() {
 
         myPlant.plantName = "" */
 
-        var flower = Plant()
+       /* var flower = Plant()
         var tree = Plant()
 
         var collectionOfPlants: ArrayList<Plant> = ArrayList()
         collectionOfPlants.add(flower)
-        collectionOfPlants.add(tree)
+        collectionOfPlants.add(tree) */
 
+        cameraButton = findViewById(R.id.btnOpenCamera)
+        photoGalleryButton = findViewById(R.id.btnOpenPhotoGallery)
+        imageTaken = findViewById(R.id.imgTaken)
+
+        cameraButton?.setOnClickListener(View.OnClickListener {
+
+            Toast.makeText(this, "THE Camera Button is Clicked", Toast.LENGTH_SHORT).show()
+
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent,OPEN_CAMERA_BUTTON_REQUEST_ID)
+        })
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == OPEN_CAMERA_BUTTON_REQUEST_ID) {
+
+            if (resultCode == Activity.RESULT_OK) {
+
+                val imageData = data?.getExtras()?.get("data") as Bitmap
+                imageTaken?.setImageBitmap(imageData)
+
+            }
+        }
     }
 
     fun button1IsClicked(buttonView: View) {
